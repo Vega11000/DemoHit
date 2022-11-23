49965ed039965e06a6406314a9e04c6f5006c06e
Ilya Kalgannov <ilya.kalganov.97@mail.ru>
2022-11-23 19:50:04 +0300

    Added foundation

---
+++ DemoHit/Navigation/NavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationController.swift
+//  DemoHit
+//
+//  Created by Илья Калганов on 23.11.2022.
+//
+
+import UIKit
+
+
+final class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ DemoHit/Navigation/TabBarController.swift
@@ -0,0 +1,65 @@
+//
+//  TabBarController.swift
+//  DemoHit
+//
+//  Created by Илья Калганов on 23.11.2022.
+//
+
+import UIKit
+
+
+final class TabBarController: UITabBarController {
+    
+    convenience init() {
+        self.init(nibName: nil, bundle: nil)
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.isOpaque = true
+        tabBar.tintColor = .black
+        tabBar.unselectedItemTintColor = .systemCyan
+        tabBar.backgroundColor = .white
+        setupTabBar()
+    }
+}
+
+private extension TabBarController {
+    
+    func setupTabBar() {
+        let navVCSlingshotLaunch = NavigationController(rootViewController: SlingshotLaunchViewController())
+        let navVCLineLaunch = NavigationController(rootViewController: LineLaunchViewController())
+        
+        let configuration = UIImage.SymbolConfiguration(
+            pointSize: 20, weight: .semibold
+        )
+        
+        navVCSlingshotLaunch.tabBarItem = UITabBarItem(
+            title: "SlingshotLaunch",
+            image: UIImage(systemName: "arrowtriangle.up.circle"),
+            tag: 0
+        )
+        navVCSlingshotLaunch.tabBarItem.selectedImage = UIImage(systemName: "arrowtriangle.up.circle",
+                                                                          withConfiguration: configuration)
+        
+        navVCLineLaunch.tabBarItem = UITabBarItem(
+            title: "LineLaunch",
+            image: UIImage(systemName: "line.diagonal"),
+            tag: 1
+        )
+        navVCLineLaunch.tabBarItem.selectedImage = UIImage(systemName: "line.diagonal",
+                                                                          withConfiguration: configuration)
+        
+        setViewControllers([
+            navVCSlingshotLaunch, navVCLineLaunch
+        ], animated: false)
+    }
+}
